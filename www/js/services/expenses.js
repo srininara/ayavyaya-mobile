e8baabeec59b080f21fa80514d68c74da9b8ba39
Srinivas Narayanan <srininara@gmail.com>
2016-11-07 23:27:13 +0530

    Tag support
    Expenses with tags - addition, removal, update etc.

--- www/js/services/expenses.js
+++ www/js/services/expenses.js
@@ -15,6 +15,51 @@ angular.module('ayavyaya.services.expenseService', ['ayavyaya.config',
   ])
   .service('ExpensesService', function($q, $filter, ExpenseDataAccessService) {
 
+    var createTagStrFromTags = function(tags) {
+      var tagStr = "";
+      angular.forEach(tags, function(tag, index) {
+        if (index > 0) {
+          tagStr = tagStr + ", "
+        }
+        tagStr = tagStr + tag.name
+      });
+      return tagStr;
+    };
+
+    var addTagStrToExpenses = function(expenses) {
+      angular.forEach(expenses, function (expense, index) {
+        expense.tagStr = createTagStrFromTags(expense.tags);
+      });
+      return expenses;
+    };
+
+    var createTag = function(expense) {
+      var tagStr = expense.tagStr;
+      var tags = expense.tags;
+      if (tagStr) {
+        var tagNames = tagStr.split(",");
+        var tagsMap = {};
+        var tagsDerived = []
+        for (var tagIndex in tags) {
+          tagsMap[tags[tagIndex].name] = tags[tagIndex].id
+        }
+        for (var tagNameIndex in tagNames) {
+          var tagName = tagNames[tagNameIndex].trim();
+          if (tagName in tagsMap) {
+            var newTag = {"id": tagsMap[tagName], "name": tagName};
+          } else {
+            console.log("tag:" + tagName);
+            var newTag = {"name": tagName};
+          }
+          tagsDerived.push(newTag);
+        }
+        expense.tags = tagsDerived;
+
+      } else {
+        expense.tags=[];
+      }
+    };
+
     var self = {
 
       'load': function(index, pageSize) {
@@ -31,7 +76,7 @@ angular.module('ayavyaya.services.expenseService', ['ayavyaya.config',
           };
           ExpenseDataAccessService.customGET("", params).then(function(
             data) {
-            d.resolve(data.expenses);
+            d.resolve(addTagStrToExpenses(data.expenses));
           }, function(err) {
             d.reject(err);
           });
@@ -43,6 +88,7 @@ angular.module('ayavyaya.services.expenseService', ['ayavyaya.config',
         var copyExpenseRec = angular.copy(expenseRec);
         copyExpenseRec.expense_date = $filter('date')(expenseRec.expense_date,
           'yyyy-MM-dd');
+        createTag(copyExpenseRec);
         if (mode === "create") {
           ExpenseDataAccessService.post(copyExpenseRec).then(function(
             addedExpense) {
